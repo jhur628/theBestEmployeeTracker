9f58da7d4f0c72840f99febe76ca61dcb58dc32d
John Hur <hur.john628@gmail.com>
2021-12-15 16:31:15 -0600

    made starting prompt

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,49 @@
 const inquirer = require('inquirer');
 
+
+const startPrompt = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a role",
+                "Add a employee",
+                "Update an employee role"
+            ]
+        }
+    ])
+    .then(data => {
+        switch (data.choice) {
+            case "View all departments":
+                // viewDepartments()
+                console.log("Viewing Departments");
+            break;
+            case "View all roles":
+                // viewRoles()
+                console.log("Viewing Roles");
+            break;
+            case "View all employees":
+                // viewEmployees()
+                console.log("Viewing Employees")
+            break;
+            case "Add a role":
+                // addRole()
+                console.log("Adding Role")
+            break;
+            case "Add a employee":
+                // addEmployee()
+                console.log("Adding Employee")
+            break;
+            case "Update an employee role":
+                // updateRole()
+                console.log("Updating employee role")
+            break;
+        }
+    })
+}
+
+module.exports = startPrompt;
--- server.js
+++ server.js
@@ -1,1 +1,30 @@
+const express = require('express');
 const mysql = require('mysql2');
+const startPrompt = require('./assets/js/index')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Create connection to mysql
+const connection = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "Password1",
+        database: "employee_db"
+    },
+    console.log("CONNECTED!")
+)
+
+startPrompt();
+
+// connection.query('DESCRIBE employee', function(err, results) {
+//     console.table(results)
+// });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
